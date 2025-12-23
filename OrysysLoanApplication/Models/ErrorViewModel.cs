d56fe7246bd7bb219d88f32afe871433ca34cfb9
sawsi <sawsi@SAWSIRI>
2025-12-23 09:07:32 +0530

    Add project files.

---
+++ OrysysLoanApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrysysLoanApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
