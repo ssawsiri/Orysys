3544cb3e0b5d1ae6fb7072fc383152cb13fd8d0b
sawsiri <ssawsiri@gmail.com>
2025-12-23 11:13:57 +0530

    Add LoginViewModel and styled login page

---
+++ OrysysLoanApplication/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrysysLoanApplication.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
